ce2f28a0103d230e7d4c432fb4c5f4c10973bf6f
kuolun <kuolun@gmail.com>
2022-09-29 15:39:37 +0800

    add loding spinner

--- static/app.js
+++ static/app.js
@@ -4,6 +4,9 @@ $("#searchForm").submit(function(event) {
   // Stop form from submitting normally
   event.preventDefault();
 
+  //將div顯示
+  $('#loading').css("display", "");
+
   // Get some values from elements on the page:
   const $form = $(this);
   const device = $form.find("input[name='device']").val();
@@ -19,6 +22,10 @@ $("#searchForm").submit(function(event) {
   // Put the results in a div
   posting.done(function(data) {
     // const content = $(data).find("#content");
+
+    //再次隱藏spinner
+    $('#loading').css("display", "none");
+
     console.log(data)
     $("#result").empty().append(data);
     // $("#result").append(content);
